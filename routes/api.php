2426530cc7a6e98bf8a3b49f83267dfdf05597cc
rigobertoalag <a20150338@utem.edu.mx>
2021-10-20 13:31:02 -0500

    prepare to heroku

--- routes/api.php
+++ routes/api.php
@@ -20,12 +20,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', 'App\Http\Controllers\UserController@login');
 Route::post('register', 'App\Http\Controllers\UserController@register');
+Route::get('test2', 'App\Http\Controllers\TestController@test2');
 
 Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::get('test', 'App\Http\Controllers\UserController@test');
     Route::post('logout', 'App\Http\Controllers\UserController@logout');
 
-    Route::get('test2', 'App\Http\Controllers\TestController@test2');
+    // Route::get('test2', 'App\Http\Controllers\TestController@test2');
     Route::get('userdatatest', 'App\Http\Controllers\TestController@userdatatest');
 
     Route::apiResource('checkin', App\Http\Controllers\CheckInController::class);
